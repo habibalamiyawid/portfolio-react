a7e68c4d709adb2e63204878927db56053762940
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-09-27 00:49:14 +0600

    hero updating

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,7 +4,7 @@ import theme_pattern from '../../assets/theme_pattern.svg'
 import profile_img from '../../assets/display_pic_1.svg'
 const About = () => {
   return (
-    <div id='about' className='about'>
+    <div id='about'className='about'>
       <div className="about-title">
         <h1>About me</h1>
         <img src={theme_pattern}  alt="" />
