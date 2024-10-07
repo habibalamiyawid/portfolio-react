6aef2e20bcb52afe945607df1369cbfdc27f8466
habibalamiyawid <habibalamiya.womenindigital@gmail.com>
2024-10-08 00:37:43 +0600

    editing services

--- src/assets/services_data.js
+++ src/assets/services_data.js
@@ -10,22 +10,25 @@ const Services_Data = [
     {
         s_no:"02",
         s_name:"Block chain Instructor",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"After completing a comprehensive Blockchain course under the EDGE hire and training program with Women In Digital.....",
+        full_desc:"After completing a comprehensive Blockchain course under the EDGE hire and training program with Women In Digital, I was chosen as a Blockchain Instructor to share my expertise. Since then, I’ve had the opportunity to teach at several prestigious universities, including North South University, Ahasanullah University, United International University, American International University, State University of Bangladesh, and European University of Bangladesh. My teaching philosophy focuses on breaking down complex Blockchain concepts and providing students with practical, hands-on experience in this rapidly evolving field."
     },
     {
         s_no:"03",
         s_name:"Cyber Security Instructor",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"As a cybersecurity instructor, I have conducted workshops aimed at helping entrepreneurs protect their...",
+        full_desc:"As a cybersecurity instructor, I have conducted workshops aimed at helping entrepreneurs protect their business pages and digital assets from cyber threats. Many business owners have faced challenges such as account hacks, phishing attacks, and other online scams, which have a direct impact on their businesses. Through my workshops, I provide practical, actionable guidance on how to safeguard online platforms, secure social media accounts, and respond effectively to cyber incidents. My goal is to empower entrepreneurs with the knowledge and tools they need to stay safe in the digital space."
     },
     {
         s_no:"04",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Effective Communicator & Event Organizer",
+        s_desc:"With strong communication skills, I have successfully conducted numerous meetings...",
+        full_desc:"With strong communication skills, I have successfully conducted numerous meetings, including client meetings on behalf of my CEO. My experience extends to organizing and leading various events and workshops in universities, ensuring smooth coordination and impactful interactions. Whether working with teams or clients, I prioritize clear communication and effective collaboration to achieve the best outcomes.I excel at bridging gaps between diverse groups, facilitating productive discussions that lead to innovative solutions. My ability to articulate ideas clearly and listen actively has been instrumental in driving projects forward. By fostering an inclusive and engaging environment, I encourage participation and inspire others to contribute their best. My passion for connecting with people and building relationships has been a cornerstone of my professional journey."
     },
     {
         s_no:"05",
         s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Web development is the process of building, programming...",
     },
     {
         s_no:"06",
